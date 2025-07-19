b160239a67b21ab9886b6922d2e665d263df0099
ReKylee <ido5490@gmail.com>
2025-07-19 13:30:31 +0300

    Added cirs()

--- src/Shared/App.ts
+++ src/Shared/App.ts
@@ -3,9 +3,10 @@ import { errorMiddleware } from './Middlewares/errorMiddleware.js';
 import authRoutes from '../Infrastructure/API/Routes/authRoutes.js';
 import noteRoutes from '../Infrastructure/API/Routes/noteRoutes.js';
 import adminRouter from '../Infrastructure/API/Routes/adminRoutes.js';
-
+import cors from 'cors';
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
