e1e5506b3f76b46f05b28d596621482d7d6c67b3
ReKylee <ido5490@gmail.com>
2025-07-19 13:37:34 +0300

    Set origin to frontend URL

--- src/Shared/App.ts
+++ src/Shared/App.ts
@@ -4,9 +4,14 @@ import authRoutes from '../Infrastructure/API/Routes/authRoutes.js';
 import noteRoutes from '../Infrastructure/API/Routes/noteRoutes.js';
 import adminRouter from '../Infrastructure/API/Routes/adminRoutes.js';
 import cors from 'cors';
+import { appBaseUrl } from './services.js';
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: appBaseUrl,
+    }),
+);
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
